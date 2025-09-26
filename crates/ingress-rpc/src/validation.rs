f40b88ff200194a4ec25dba52dc2495fef3b8d8b
William Law <williamlaw.wtl@gmail.com>
2025-09-26 10:24:29 -0400

    add err types

--- crates/ingress-rpc/src/validation.rs
+++ crates/ingress-rpc/src/validation.rs
@@ -2,9 +2,11 @@ use alloy_consensus::{Transaction, Typed2718, constants::KECCAK_EMPTY, transacti
 use alloy_primitives::{Address, B256, U256, address};
 use alloy_provider::{Provider, RootProvider};
 use anyhow::Result;
+use jsonrpsee::{core::RpcResult, types::ErrorObject};
 use op_alloy_consensus::OpTxEnvelope;
 use op_alloy_network::Optimism;
 use op_revm::{OpSpecId, l1block::L1BlockInfo};
+use reth_rpc_eth_types::{EthApiError, RpcInvalidTransactionError};
 
 // from: https://github.com/alloy-rs/op-alloy/blob/main/crates/consensus/src/interop.rs#L9
 // reference: https://github.com/paradigmxyz/reth/blob/bdc59799d0651133d8b191bbad62746cb5036595/crates/optimism/txpool/src/supervisor/access_list.rs#L39
@@ -32,16 +34,20 @@ impl AccountInfoLookup for RootProvider<Optimism> {
 }
 
 pub trait TxValidation {
-    async fn validate_tx(&self, txn: &Recovered<OpTxEnvelope>, data: &[u8]) -> Result<()>;
+    async fn validate_tx(&self, txn: &Recovered<OpTxEnvelope>, data: &[u8]) -> RpcResult<B256>;
 }
 
 impl TxValidation for RootProvider<Optimism> {
-    async fn validate_tx(&self, txn: &Recovered<OpTxEnvelope>, data: &[u8]) -> Result<()> {
-        let account = self.fetch_account_info(txn.signer()).await?;
+    async fn validate_tx(&self, txn: &Recovered<OpTxEnvelope>, data: &[u8]) -> RpcResult<B256> {
+        let account = self.fetch_account_info(txn.signer()).await.map_err(|e| {
+            let obj = ErrorObject::owned(11, e.to_string(), Some(2));
+            RpcInvalidTransactionError::other(obj).into_rpc_err()
+        })?;
 
         // skip eip4844 transactions
         if txn.is_eip4844() {
-            return Err(anyhow::anyhow!("EIP-4844 transactions are not supported"));
+            let obj = ErrorObject::owned(11, "EIP-4844 transactions are not supported", Some(2));
+            return Err(RpcInvalidTransactionError::other(obj).into_rpc_err());
         }
 
         // from: https://github.com/paradigmxyz/reth/blob/3b0d98f3464b504d96154b787a860b2488a61b3e/crates/optimism/txpool/src/supervisor/client.rs#L76-L84
@@ -52,21 +58,29 @@ impl TxValidation for RootProvider<Optimism> {
                 .iter()
                 .filter(|entry| entry.address == CROSS_L2_INBOX_ADDRESS);
             if inbox_entries.count() > 0 {
-                return Err(anyhow::anyhow!("Interop transactions are not supported"));
+                let obj = ErrorObject::owned(11, "Interop transactions are not supported", Some(2));
+                return Err(RpcInvalidTransactionError::other(obj).into_rpc_err());
             }
         }
 
         // error if account is 7702 but tx is not 7702
         if account.code_hash != KECCAK_EMPTY && !txn.is_eip7702() {
-            return Err(anyhow::anyhow!(
-                "Account is a 7702 account but transaction is not EIP-7702"
-            ));
+            return Err(EthApiError::InvalidTransaction(
+                RpcInvalidTransactionError::AuthorizationListInvalidFields,
+            )
+            .into_rpc_err());
         }
 
         // error if tx nonce is not the latest
         // https://github.com/paradigmxyz/reth/blob/a047a055ab996f85a399f5cfb2fe15e350356546/crates/transaction-pool/src/validate/eth.rs#L611
         if txn.nonce() < account.nonce {
-            return Err(anyhow::anyhow!("Nonce is not the latest"));
+            return Err(
+                EthApiError::InvalidTransaction(RpcInvalidTransactionError::NonceTooLow {
+                    tx: txn.nonce(),
+                    state: account.nonce,
+                })
+                .into_rpc_err(),
+            );
         }
 
         // For EIP-1559 transactions: `max_fee_per_gas * gas_limit + tx_value`.
@@ -78,7 +92,10 @@ impl TxValidation for RootProvider<Optimism> {
 
         // error if execution cost costs more than balance
         if txn_cost > account.balance {
-            return Err(anyhow::anyhow!("Insufficient funds"));
+            return Err(EthApiError::InvalidTransaction(
+                RpcInvalidTransactionError::InsufficientFundsForTransfer,
+            )
+            .into_rpc_err());
         }
 
         // op-checks to see if sender can cover L1 gas cost
@@ -87,8 +104,11 @@ impl TxValidation for RootProvider<Optimism> {
         let l1_cost_addition = l1_block_info.calculate_tx_l1_cost(data, OpSpecId::ISTHMUS);
         let l1_cost = txn_cost.saturating_add(l1_cost_addition);
         if l1_cost > account.balance {
-            return Err(anyhow::anyhow!("Insufficient funds to cover L1 gas cost"));
+            return Err(EthApiError::InvalidTransaction(
+                RpcInvalidTransactionError::InsufficientFundsForTransfer,
+            )
+            .into_rpc_err());
         }
-        Ok(())
+        Ok(txn.tx_hash())
     }
 }
