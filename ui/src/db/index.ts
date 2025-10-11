3e70d939a3873bf5672ca925c191130f25111bd7
Danyal Prout <git@dany.al>
2025-10-11 11:37:48 -0500

    fix: misc fixes + logging (#27)
    * fix: connecting to rds pg cluster
    
    * add error logging
    
    * add additional logging
    
    * add logging
    
    * persist to db

--- ui/src/db/index.ts
+++ ui/src/db/index.ts
@@ -4,6 +4,10 @@ import * as schema from "./schema";
 
 const pool = new Pool({
   connectionString: process.env.TIPS_DATABASE_URL,
+  ssl: {
+    requestCert: false,
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool, { schema });
