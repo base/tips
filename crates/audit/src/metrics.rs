e399f047c39207e158da74fc8c15d5b0df2fee3a
Cody Wang <cody.wang@coinbase.com>
2025-12-15 15:57:24 -0500

    audit metrics

---
+++ crates/audit/src/metrics.rs
@@ -0,0 +1,45 @@
+use metrics::{Counter, Histogram};
+use metrics_derive::Metrics;
+use metrics_exporter_prometheus::PrometheusBuilder;
+use std::net::SocketAddr;
+
+#[derive(Metrics, Clone)]
+#[metrics(scope = "tips_audit")]
+pub struct Metrics {
+    #[metric(describe = "Duration of archive_event")]
+    pub archive_event_duration: Histogram,
+
+    #[metric(describe = "Age of event when processed (now - event timestamp)")]
+    pub event_age: Histogram,
+
+    #[metric(describe = "Duration of Kafka read_event")]
+    pub kafka_read_duration: Histogram,
+
+    #[metric(describe = "Duration of Kafka commit")]
+    pub kafka_commit_duration: Histogram,
+
+    #[metric(describe = "Duration of update_bundle_history")]
+    pub update_bundle_history_duration: Histogram,
+
+    #[metric(describe = "Duration of update all transaction indexes")]
+    pub update_tx_indexes_duration: Histogram,
+
+    #[metric(describe = "Duration of S3 get_object")]
+    pub s3_get_duration: Histogram,
+
+    #[metric(describe = "Duration of S3 put_object")]
+    pub s3_put_duration: Histogram,
+
+    #[metric(describe = "Total events processed")]
+    pub events_processed: Counter,
+
+    #[metric(describe = "Total S3 writes skipped due to dedup")]
+    pub s3_writes_skipped: Counter,
+}
+
+pub fn init_prometheus_exporter(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    PrometheusBuilder::new()
+        .with_http_listener(addr)
+        .install()
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)
+}
